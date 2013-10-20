5aef53209d0ff53a8aac099d0d59d733f7817b2f
salarkhan <salar.k3@gmail.com>
2013-10-20 14:57:24 -0700

    suck it twice

--- public/javascripts/racer.js
+++ public/javascripts/racer.js
@@ -8,36 +8,45 @@ function racer(){
 		((e.keyCode))
 	}
 
-	//on load, get class hella hidden, get string, split
-	var	gameText = document.getElementById("gameText").innerText
-
-	function stringSplitter(text){
-		return text.split("")
+	//on load, get string, split
+	function stringSplitter(){
+		var	gameText = document.getElementById("gameText").innerHTML
+		return gameText.split("")
 	}
 
-	//wrap each character in span
+	//wrap each character in span, join to remove the commas
 	function stringSpanner(splitText){
 		for(i=0; i<splitText.length; i++){
-			splitText[i] = '<span>' + i + '</span>'
+			splitText[i] = '<span>' + splitText[i] + '</span>'
 		}
-		return splitText
+		return splitText.join('')
 	}
 
 	//take the spanned string and replace the innerHTML 
 	function stringReplacer(){
-		gameText = stringSpanner(stringSplitter(gameText));
+		document.getElementById("gameText").innerHTML = stringSpanner(stringSplitter());
 	}
 
-	function childAppender(){
-		test = document.getElementById("spannedGameText")
-		console.log(test)
-		console.log(gameText)
-		test.appendChild(gameText)
-	}
+	//the above 3 methods in one line. #brunostatus #stackoverflowcomments
+	//element.innerHTML='<span>'+textNode.data.split('').join('<\/span><span>'+'<\/span>')
 
 
 	//better idea. create spans, iterate through string and stick them in.
-	
+
+	//documentFragment is a DOM node--it's never a part of the main DOM tree. the usual way they are utilized: create the frag, append elements to it, and then append the frag to the tree. in the tree, the frag is replaced by all of it's children.
+
+	//regarding text nodes/nodeValue--If you want to return the text of an element, remember that text is always inside a Text node, and you will have to return the Text node's node value (element.childNodes[0].nodeValue).
+
+	var	gameText = document.getElementById("gameText")
+	var documentFrag = document.createDocumentFragment();
+	for(var i=0, stringLength=gameText.innerHTML.length; i<stringLength; i++){
+		var span = document.createElement('span')
+		span.innerHTML = gameText.innerHTML.charAt(i)
+		documentFrag.appendChild(span)
+	}	
+	gameText.parentNode.replaceChild(documentFrag,gameText)
+
+
 
 	function stringKeyCodeGetter(){	
 	}
@@ -48,9 +57,8 @@ function racer(){
 	function keyCodeMatcher(){	
 	}
 
-	stringReplacer()
-	console.log(childAppender())
-	// console.log(gameText)
+	// stringReplacer()
+	// console.log(document.getElementById("gameText").innerHTML)
 
 
 }
