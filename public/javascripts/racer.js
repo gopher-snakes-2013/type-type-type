3067703c623f1833c4632f601d3c52567909a455
salarkhan <salar.k3@gmail.com>
2013-10-20 16:34:59 -0700

    added keycode getters

--- public/javascripts/racer.js
+++ public/javascripts/racer.js
@@ -1,12 +1,11 @@
 function racer(){
 
 	function bindEventListeners(){}
-	document.addEventListener("keypress",whatKey,false)	
+	document.addEventListener("keypress",inputKeyCodeGetter,false)	
 
-	//grab keycode of event
-	function whatKey(e){
-		((e.keyCode))
-	}
+
+	//below 3 methods in one line. #brunostatus 
+	//element.innerHTML='<span>'+textNode.data.split('').join('<\/span><span>'+'<\/span>')
 
 	//on load, get string, split
 	function stringSplitter(){
@@ -21,43 +20,49 @@ function racer(){
 		}
 		return splitText.join('')
 	}
+	//assign splitter to var 
+
 
 	//take the spanned string and replace the innerHTML 
 	function stringReplacer(){
 		document.getElementById("gameText").innerHTML = stringSpanner(stringSplitter());
 	}
 
-	//the above 3 methods in one line. #brunostatus #stackoverflowcomments
-	//element.innerHTML='<span>'+textNode.data.split('').join('<\/span><span>'+'<\/span>')
 
 
-	//better idea. create spans, iterate through string and stick them in.
+	//different idea. create spans, iterate through string and stick them in.
+
+	//regarding text nodes/nodeValue--If you want to return the text of an element, remember that text is always inside a Text node, and you will have to return the Text node's node value (element.childNodes[0].nodeValue). *!*!* nodeValue only works for text , returns null for other elements
 
 	//documentFragment is a DOM node--it's never a part of the main DOM tree. the usual way they are utilized: create the frag, append elements to it, and then append the frag to the tree. in the tree, the frag is replaced by all of it's children.
+	function fragmentSpanner(){
+		var	gameText = document.getElementById("gameText")
+		var documentFrag = document.createDocumentFragment();
 
-	//regarding text nodes/nodeValue--If you want to return the text of an element, remember that text is always inside a Text node, and you will have to return the Text node's node value (element.childNodes[0].nodeValue).
+		for(var i=0, stringLength=gameText.innerHTML.length; i<stringLength; i++){
+			var span = document.createElement('span')
+			span.innerHTML = gameText.innerHTML.charAt(i)
+			documentFrag.appendChild(span)
+		}	
 
-	var	gameText = document.getElementById("gameText")
-	var documentFrag = document.createDocumentFragment();
-	for(var i=0, stringLength=gameText.innerHTML.length; i<stringLength; i++){
-		var span = document.createElement('span')
-		span.innerHTML = gameText.innerHTML.charAt(i)
-		documentFrag.appendChild(span)
-	}	
-	gameText.parentNode.replaceChild(documentFrag,gameText)
-
-
-
-	function stringKeyCodeGetter(){	
+		gameText.parentNode.replaceChild(documentFrag,gameText)
 	}
 
-	function inputKeyCodeGetter(){
+	//grab keycode of character
+	function stringKeyCodeGetter(character){	
+		return character.keyCode
 	}
+
+	//grab keycode of event
+	function inputKeyCodeGetter(event){
+		return event.keyCode
+	}
+	
 
 	function keyCodeMatcher(){	
 	}
 
-	// stringReplacer()
+	stringReplacer()
 	// console.log(document.getElementById("gameText").innerHTML)
 
 
---
+++ spec/javascripts/racerSpec.js
@@ -0,0 +1,11 @@
+//do i have a homepage
+//do i have a form for input. create textarea
+//wpm
+//capybara test begin game
+//did i append anything to the dom
+
+describe('whatKey',function(){
+  it('should fire',function(){
+    expect(whatKey).toBe('happy');
+  });
+});
