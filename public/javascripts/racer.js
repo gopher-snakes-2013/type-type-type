9f631bfb10e2f3584083f53605245123f467cb84
salarkhan <salar.k3@gmail.com>
2013-10-20 00:12:52 -0700

    wrote pseudocode-ish functions for spanning and matching

--- public/javascripts/racer.js
+++ public/javascripts/racer.js
@@ -1,18 +1,44 @@
-document.addEventListener("keypress",whatKey,false)
+function racer(){
+
+function bindEventListeners(){}
+document.addEventListener("keypress",whatKey,false)	
 
 function whatKey(e){
-	(e.keyCode)
+	((e.keyCode))
 }
 
-var text = document.getElementById("text")
-var numberOfLetters = text.children.length
-//tester = document.querySelectorAll("#test.c_test").length;
-for(i=0; i<numberOfLetters; i++){}
 
 
-
-//write a function that will compare keycode against
-//keycode of letter in html
 
 //on load, get class hella hidden, get string, count, clone
 //iterate through, split, wrap each in span and append it to the div
+function stringSplitter(){
+	var	string = document.getElementById("hella_hidden").innerText
+	return string.split("")
+}
+
+function stringSpanner(text){
+	for(i=0; i<text.length; i++){
+		text[i] = '<span>' + i + '</span>'
+	}
+}
+var spanned_string = stringSpanner(stringSplitter());
+
+function stringReplacer(spanned_string){
+	//take the spanned string and replace the innerHTML
+}
+
+function stringKeyCodeGetter(){	
+}
+
+function inputKeyCodeGetter(){
+}
+
+function keyCodeMatcher(){	
+}
+
+
+
+}
+
+window.addEventListener('load',racer,false)
