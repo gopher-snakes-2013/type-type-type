e9a4acd52fbb0abef573468f66ac850a63777640
salarkhan <salar.k3@gmail.com>
2013-10-19 23:34:08 -0700

    rao idea: wrap each letter in span on load

--- public/javascripts/racer.js
+++ public/javascripts/racer.js
@@ -5,12 +5,14 @@ function whatKey(e){
 }
 
 var text = document.getElementById("text")
+var numberOfLetters = text.children.length
 //tester = document.querySelectorAll("#test.c_test").length;
-
-
-// for(i=0; i<; i++){}
+for(i=0; i<numberOfLetters; i++){}
 
 
 
 //write a function that will compare keycode against
 //keycode of letter in html
+
+//on load, get class hella hidden, get string, count, clone
+//iterate through, split, wrap each in span and append it to the div
