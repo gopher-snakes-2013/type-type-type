7431dbcbc18c70b7b8e9aa2cb0691f132c2652ff
salarkhan <salar.k3@gmail.com>
2013-10-20 23:34:44 -0700

    deleted extraneous jasmine files

--- public/javascripts/racer.js
+++ public/javascripts/racer.js
@@ -1,5 +1,6 @@
 function racer(){
-		
+	var	gameText = document.getElementById("gameText")
+
 
 	function bindEventListeners(){}
 	document.addEventListener("keypress",advanceCharacter,false)	
@@ -38,7 +39,7 @@ function racer(){
 
 	//documentFragment is a DOM node--it's never a part of the main DOM tree. the usual way they are utilized: create the frag, append elements to it, and then append the frag to the tree. in the tree, the frag is replaced by all of it's children.
 	function fragmentSpanner(){
-		var	gameText = document.getElementById("gameText")
+		// var	gameText = document.getElementById("gameText")
 		var documentFrag = document.createDocumentFragment();
 
 
@@ -52,33 +53,25 @@ function racer(){
 	}
 //************************************************************************	
 
-	//grab keycode of character
-	function stringKeyCodeGetter(character){	
-		return character.keyCode
-	}
-
-	//grab keycode of event
+	//grab keyCode of keypress event
 	function inputKeyCodeGetter(event){
 		// console.log(event.keyCode)
 		return event.keyCode
 	}
 	
-
-	function keyCodeMatcher(event){	
-		inputKeyCodeGetter(event)
+	//grab char within gametext, change to charCode
+	function characterGetter(){
+		// var	gameText = document.getElementById("gameText")
+		return gameText.innerText.substring(correctCounter-1,correctCounter).charCodeAt()
 	}
-
 
 	var correctCounter = 1
 	function advanceCharacter(event){
-		var	gameText = document.getElementById("gameText")
 		var selectedSpan = gameText.children[correctCounter - 1]
-		//break character out into it's own function
-		var character = gameText.innerText.substring(correctCounter-1,correctCounter).charCodeAt()
-
+		var character = characterGetter()
 
 		inputKeyCodeGetter(event)
-	
+
 		if(character == inputKeyCodeGetter(event)){
 			selectedSpan.id = "highlight"
 			correctCounter += 1
